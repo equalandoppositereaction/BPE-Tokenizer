a2222a5c4cf7f5837487aed5a3293e5ed4569b44
Cyan <sharestuffhehe@gmail.com>
2026-03-09 11:21:53 +0530

    commiting while the tokenizer trains to increase daily commits :)

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 #idk what to do here
+from nitok import Tokenizer
+#import multiprocessing
+with open("1984.txt", encoding="utf-8") as f:
+    text = f.read()
+tok = Tokenizer()
+tok.train(text, 5000, True) #very shitty, want multiprocessing, dont know multi processing, me sad :( but still no AI caus fuck AI :)
+
+#Validation
+valtext = "Natural language processing tasks, such as question answering, machine translation, reading comprehension, and summarization, are typicallyapproached with supervised learning on taskspecific datasets."
+valtest = tok.decode(tok.encode(valtext))
+print(valtext == valtest)
+
+encoded = tok.encode("One of them was a girl whom he often passed in the corridors")
+for x in encoded:
+  print(tok.decode([x]))
--- nitok.py
+++ nitok.py
@@ -8,13 +8,13 @@ class Tokenizer: #not the brightest when it comes to OOPS I suppose
         self.ids = []
         self.vocab_size = None
 
-    def mostfreq(tokens):
+    def mostfreq(self, tokens):
         counts = {}
         for pair in zip(tokens, tokens[1:]):
             counts[pair] = counts.get(pair, 0) + 1
         return counts
     
-    def merge(ids, pair, idx): 
+    def merge(self, ids, pair, idx):
         newids = [] 
         i = 0 
         while i < len(ids): 
@@ -26,7 +26,7 @@ class Tokenizer: #not the brightest when it comes to OOPS I suppose
                 i+=1
         return newids
     
-    def pretok(text):
+    def pretok(self, text):
         #from GPT-4, raw string of splited raw text 
         ptr = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+"""
         out = re.compile(ptr)
@@ -37,7 +37,7 @@ class Tokenizer: #not the brightest when it comes to OOPS I suppose
         
         self.vocab_size = vocab_size
         
-        text = pretok(text)
+        text = "".join(self.pretok(text))
 
         tokens = text.encode('utf-8')
         tokens = list(map(int, tokens))
@@ -45,12 +45,12 @@ class Tokenizer: #not the brightest when it comes to OOPS I suppose
         self.ids = list(tokens)
 
         for i in range(num_merges):
-            stats = mostfreq(self.ids)
+            stats = self.mostfreq(self.ids)
             pair = max(stats, key = stats.get)
             idx = 256 + i
             if verbose:
                 print(f"merge {pair} into {idx}")
-            self.ids = merge(self.ids, pair, idx)
+            self.ids = self.merge(self.ids, pair, idx)
             self.merges[pair] = idx
         
         self.vocab = {idx: bytes([idx]) for idx in range(256)}
@@ -67,10 +67,10 @@ class Tokenizer: #not the brightest when it comes to OOPS I suppose
     def encode(self, text):
         tokens = list(text.encode('utf-8'))
         while len(tokens) >= 2:
-            stats = mostfreq(tokens)
+            stats = self.mostfreq(tokens)
             pair = min(stats, key=lambda p: self.merges.get(p, float("inf")))
             if pair not in self.merges:
                 break
             idx = self.merges[pair]
-            tokens = merge(tokens, pair, idx)
+            tokens = self.merge(tokens, pair, idx)
         return tokens
